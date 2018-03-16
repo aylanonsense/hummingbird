3ad0ea6a0a8042763d98f4f8f4ede5cf64ce4791
bridgs <bridgs.dev@gmail.com>
2018-03-16 12:05:00 -0400

    Adding a basic index.html file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+import express from 'express';
+
+// create a server
+const app = express();
+app.use(express.static('public'));
+
+// start the server
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`Listening on port ${port}`);
